d5f764fd0bc833ced6c846b504f0a879feefe800
dlagogo <tpals7631@naver.com>
2022-02-19 14:01:41 +0900

    즐겨찾기,삭제하기 기능 추가된 index.html, app.py
    즐겨찾기,삭제하기 기능 추가완료
    *카드 높이 안맞음 수정해야함
    *즐겨찾기 상위 랭크 안되는 문제 수정해야함

--- app.py
+++ app.py
@@ -13,27 +13,6 @@ db = client.dbsparta
 
 ## 스파르타 강의 4-13 뼈대
 
-
-## HTML을 주는 부분
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-#@app.route('/memo', methods=['GET'])
-#def listing():
-    #sample_receive = request.args.get('sample_give')
-    #print(sample_receive)
-    #return jsonify({'msg': 'GET 연결되었습니다!'})
-
-
-## API 역할을 하는 부분
-#@app.route('/memo', methods=['POST'])
-#def saving():
- #   sample_receive = request.form['sample_give']
-  #  print(sample_receive)
-   # return jsonify({'msg': '등록되었습니다!'})
-
 ## 즐겨찾기,삭제하기
 
 # API 역할을 하는 부분
